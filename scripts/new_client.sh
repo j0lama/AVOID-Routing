54cf06113828ec735bdc322431fb2a4dd01c5339
Jon Larrea <jon.larrea@ed.ac.uk>
2023-09-11 16:44:36 -0700

    end-to-end testing

--- scripts/new_client.sh
+++ scripts/new_client.sh
@@ -2,13 +2,14 @@
 
 MACHINE_NAME=$(hostname -s)
 GW_INDEX=${MACHINE_NAME#"gateway"}
-CLIENT_INDEX=$(cat /local/repository/config/avoid.conf | grep "[Peer]" | wc -l)
+CLIENT_INDEX=$(cat /local/repository/config/avoid.conf | grep "\[Peer\]" | wc -l)
 CLIENT_INDEX=$((CLIENT_INDEX + 2))
 CLIENT_IP="1.0.$GW_INDEX.$CLIENT_INDEX"
 INTERNET_IFACE=$(route | grep '^default' | grep -o '[^ ]*$')
 GW_IP=$(ifconfig $INTERNET_IFACE | grep 'inet ' | awk '{ print $2}')
 GW_PUB_KEY=$(cat /local/repository/config/gw_pub)
 PRIV_KEY=$(wg genkey)
+PUB_KEY=$(echo $PRIV_KEY | wg pubkey)
 
 cp /local/repository/config/client.conf /tmp/client.conf
 
@@ -21,6 +22,6 @@ sed -i "s/GW_IP/$GW_IP/g" /tmp/client.conf
 cat /tmp/client.conf | qrencode -t ansiutf8 
 
 # Add Peer entry to GW config file
-#[Peer]
-#PublicKey = <contents-of-client-publickey>
-#AllowedIPs = 10.0.0.2/32
+printf "[Peer]\n" >> /local/repository/config/avoid.conf
+printf "PublicKey = %s\n" "$PUB_KEY" >> /local/repository/config/avoid.conf
+printf "AllowedIPs = %s\n\n" "$CLIENT_IP" >> /local/repository/config/avoid.conf
