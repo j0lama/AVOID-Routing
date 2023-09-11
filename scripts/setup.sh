4de25833f99017eb55fd4bef398c52eb931d7b61
Jon Larrea <jon.larrea@ed.ac.uk>
2023-09-11 11:11:16 -0700

    Manual setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,13 +8,13 @@ sudo apt update -y
 sudo apt install -y wireguard
 
 if [ $1 = "gw" ]; then
-    git clone https://j0lama:$2@github.com/j0lama/avoid_gw.git
-    cd avoid_gw/
-    ./initialize.sh
+    #git clone https://j0lama:$2@github.com/j0lama/avoid_gw.git
+    #cd avoid_gw/
+    #./initialize.sh
 elif [ $1 = "home_gw" ]; then
-    git clone https://j0lama:$2@github.com/j0lama/avoid_home_gw.git
-    cd avoid_home_gw/
-    ./initialize.sh
+    #git clone https://j0lama:$2@github.com/j0lama/avoid_home_gw.git
+    #cd avoid_home_gw/
+    #./initialize.sh
 else
   echo "Invalid option"
   exit 1
